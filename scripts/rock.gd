055805d587f9f47670303e8ffa04cbc0fd340480
Batyrkhan Tursumbekov <tumarbatr@gmail.com>
2025-09-29 23:17:16 +0500

    Add rock explosion and smaller rock spawning

--- scripts/rock.gd
+++ scripts/rock.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+signal exploded
+
 var screensize = Vector2.ZERO
 var size
 var radius
@@ -22,3 +24,15 @@ func start(_position, _velocity, _size) -> void:
 	$CollisionShape2D.shape = shape
 	linear_velocity = _velocity
 	angular_velocity = randf_range(-PI, PI)
+	$Explosion.scale = Vector2.ONE * 0.75 * size
+
+func explode() -> void:
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	$Explosion/AnimationPlayer.play("explosion")
+	$Explosion.show()
+	exploded.emit(size, radius, position, linear_velocity)
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	await $Explosion/AnimationPlayer.animation_finished
+	queue_free()
